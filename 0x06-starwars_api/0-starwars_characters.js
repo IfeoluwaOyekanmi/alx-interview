dc9b07cd92c4d7198e30292c9492bbf02cb6b518
Ifeoluwa oyekanmi <IfeoluwaOyekanmi@users.noreply.github.com>
2024-06-07 19:44:08 +0100

    0-starwars_characters.js

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,35 +1,53 @@
 #!/usr/bin/node
+
 const request = require('request');
+
 const movieId = process.argv[2];
+const filmEndPoint = 'https://swapi-api.hbtn.io/api/films/' + movieId;
+let people = [];
+const names = [];
 
-function fetch (url) {
-  return new Promise((resolve, reject) => {
-    request(url, (error, res, body) => {
-      if (error) {
-        reject(error);
-      } else {
-        resolve(JSON.parse(body));
-      }
-    });
-  });
-}
-
-const movieUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
-
-async function getMovieCharacters () {
-  try {
-    const characters = [];
-    const movieData = await fetch(movieUrl);
-    for (const url of movieData.characters) {
-      const characterData = await fetch(url);
-      characters.push(characterData.name);
+const requestCharacters = async () => {
+  await new Promise(resolve => request(filmEndPoint, (err, res, body) => {
+    if (err || res.statusCode !== 200) {
+      console.error('Error: ', err, '| StatusCode: ', res.statusCode);
+    } else {
+      const jsonBody = JSON.parse(body);
+      people = jsonBody.characters;
+      resolve();
     }
-    characters.forEach(element => {
-      console.log(element);
-    });
-  } catch (error) {
-    console.error(error);
-  }
-}
+  }));
+};
 
-getMovieCharacters();
+const requestNames = async () => {
+  if (people.length > 0) {
+    for (const p of people) {
+      await new Promise(resolve => request(p, (err, res, body) => {
+        if (err || res.statusCode !== 200) {
+          console.error('Error: ', err, '| StatusCode: ', res.statusCode);
+        } else {
+          const jsonBody = JSON.parse(body);
+          names.push(jsonBody.name);
+          resolve();
+        }
+      }));
+    }
+  } else {
+    console.error('Error: Got no Characters for some reason');
+  }
+};
+
+const getCharNames = async () => {
+  await requestCharacters();
+  await requestNames();
+
+  for (const n of names) {
+    if (n === names[names.length - 1]) {
+      process.stdout.write(n);
+    } else {
+      process.stdout.write(n + '\n');
+    }
+  }
+};
+
+getCharNames();
